f23bf1b04189f55763714abffe9ae6349b2dc8fa
rajkumar <rajkumarsofficial@gmail.com>
2025-05-31 14:18:49 +0530

    Initial commit

---
+++ app/src/main/java/com/build/kbites_demo/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.build.kbites_demo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.build.kbites_demo.screen.BakingScreen
+import com.build.kbites_demo.ui.theme.KBites_DemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KBites_DemoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    BakingScreen()
+                }
+            }
+        }
+    }
+}
